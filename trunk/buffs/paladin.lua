443fc3f4e7508019b880a3c29e945d36cb2f845f
adirelle <adirelle@976ebace-77c9-427b-a127-ccb7ca2dd15e>
2008-10-18 20:00:42 +0000

    Paladin: removed 2.x blessings.

--- trunk/buffs/paladin.lua
+++ trunk/buffs/paladin.lua
@@ -5,16 +5,12 @@ SimpleSelfRebuff:RegisterBuffSetup(function(self, spellNames, L)
 
 	-- subcast is used to group blessings
 	self:GetCategory(L['Blessing'])
-		:add( spellNames[20217], 'subcat', 'kings'     ) -- Blessing of kight
-		:add( spellNames[19977], 'subcat', 'light'     ) -- Blessing of Light
+		:add( spellNames[20217], 'subcat', 'kings'     ) -- Blessing of Kings
 		:add( spellNames[19740], 'subcat', 'might'     ) -- Blessing of Might
-		:add( spellNames[ 1038], 'subcat', 'salvation' ) -- Blessing of Salvation
 		:add( spellNames[20911], 'subcat', 'sanctuary' ) -- Blessing of Sanctuary
 		:add( spellNames[19742], 'subcat', 'wisdom'    ) -- Blessing of Wisdom
 		:add( spellNames[25898], 'subcat', 'kings'     ) -- Greater Blessing of Kings
-		:add( spellNames[25890], 'subcat', 'light'     ) -- Greater Blessing of Light
 		:add( spellNames[25782], 'subcat', 'might'     ) -- Greater Blessing of Might
-		:add( spellNames[25895], 'subcat', 'salvation' ) -- Greater Blessing of Salvation
 		:add( spellNames[25899], 'subcat', 'sanctuary' ) -- Greater Blessing of Sanctuary
 		:add( spellNames[25894], 'subcat', 'wisdom'    ) -- Greater Blessing of Wisdom
 	
