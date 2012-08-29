697a0005f9e607e3a7b5143cdebfceb22a6d3730
durcyn <durcyn@976ebace-77c9-427b-a127-ccb7ca2dd15e>
2012-08-29 16:50:49 +0000

    priest: new pw:fort spellid

--- trunk/Buffs/priest.lua
+++ trunk/Buffs/priest.lua
@@ -4,8 +4,8 @@ if select(2, UnitClass('player')) ~= 'PRIEST' then return end
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
 	
 	self:GetCategory(L['Fortitude']):addMulti(
-		79105,  -- Power Word: Fortitude
-        6307,   -- Blood Pact
+		21562,  -- Power Word: Fortitude
+	        6307,   -- Blood Pact
 		469,    -- Commanding Shout
 		46302   -- K'iru's Song of Victory -- can not be cast by player but conflicts with PW:F/PF		
 	)
