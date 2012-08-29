ea40387fae1a828368a6b4741a5160b1a5ae39f5
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2012-08-29 15:13:38 +0000

    some class updates, paladin, priest and warlock

--- Buffs/paladin.lua
+++ Buffs/paladin.lua
@@ -8,15 +8,7 @@ SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
 		:add( 20217, 'subcat', 'kings'     ) -- Blessing of Kings
 		:add( 19740, 'subcat', 'might'     ) -- Blessing of Might
 			
-	self:GetCategory(L['Aura'])
-		:add( 465, 'subcat', 'devotion',      'mountFriendly', true ) -- Devotion Aura
-		:add( 7294, 'subcat', 'retribution',   'mountFriendly', true ) -- Retribution Aura
-		:add( 19891, 'subcat', 'resist',      'mountFriendly', true ) -- Resistance Aura
-		:add( 19746, 'subcat', 'concentration', 'mountFriendly', true ) -- Concentration Aura
-		:add( 32223, 'subcat', 'crusader',      'mountFriendly', true ) -- Crusader Aura
-
 	self:GetCategory(L['Seal']):addMulti( 
-		20164, -- Seal of Justice
 		20165, -- Seal of Insight
         20154, -- Seal of Righteousness
 		31801 -- Seal of Truth
