5a89bb1ae53ce72b12aa18ab52b1e63ccc3728d5
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2012-08-30 23:11:27 +0000

    add presences to dk module

--- Buffs/deathknight.lua
+++ Buffs/deathknight.lua
@@ -4,4 +4,10 @@ if select(2, UnitClass('player')) ~= 'DEATHKNIGHT' then return end
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
     self:AddStandaloneBuff(49222) -- Bone Shield
     self:AddStandaloneBuff(57330) -- Horn of Winter
+	
+	self:GetCategory("Presence"):addMulti(
+		48263, -- Blood Presence
+		48266, -- Frost Presence
+		48265, -- Unholy Presence
+	)
 end) 
