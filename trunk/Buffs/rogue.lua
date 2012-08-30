edcd5e19a0216a70bae938e1e1a997ac2eb91e35
durcyn <durcyn@976ebace-77c9-427b-a127-ccb7ca2dd15e>
2012-08-30 22:47:52 +0000

    change rogue poison categories from builtin MAINHAND/OFFHAND strings, since those are hardcoded to scan tooltips for imbues.  rogue module works

--- trunk/Buffs/rogue.lua
+++ trunk/Buffs/rogue.lua
@@ -3,12 +3,12 @@ if select(2, UnitClass('player')) ~= 'ROGUE' then return end
 
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
 
-	self:GetCategory(self.CATEGORY_MAINHAND):addMulti(
+	self:GetCategory("Lethal"):addMulti(
 		2823, -- Deadly Poison
-		7679  -- Wound Poison
+		8679  -- Wound Poison
 	)
 
-	self:GetCategory(self.CATEGORY_OFFHAND):addMulti(
+	self:GetCategory("Non-Lethal"):addMulti(
 		3408, -- Crippling Poison 
 		5761  -- Mind-Numbing Poison
 	)
