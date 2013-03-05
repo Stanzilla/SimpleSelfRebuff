78394b3b2837a2de346670e6bc985758aedd1fdd
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2013-03-05 02:07:42 +0000

    I'm in your toc, updating your Interface Version to 50200.

--- Buffs/paladin.lua
+++ Buffs/paladin.lua
@@ -3,7 +3,7 @@ if select(2, UnitClass('player')) ~= 'PALADIN' then return end
 
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
 
-	-- subcast is used to group blessings
+	-- subcat is used to group blessings
 	self:GetCategory(L['Blessing'])
 		:add( 20217, 'subcat', 'kings'     ) -- Blessing of Kings
 		:add( 19740, 'subcat', 'might'     ) -- Blessing of Might
