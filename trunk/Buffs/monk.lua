4c3d20e6799f4eb4c79b8bd13fa086fa8eb9c10a
stanzilla <stanzilla@976ebace-77c9-427b-a127-ccb7ca2dd15e>
2014-10-14 15:02:59 +0000

    Monks now buff either stats or stats+crit

--- trunk/Buffs/monk.lua
+++ trunk/Buffs/monk.lua
@@ -4,18 +4,18 @@ if not SimpleSelfRebuff then return end
 if select(2, UnitClass('player')) ~= 'MONK' then return end
 
 local stats = GetSpellInfo(115921)
-local mastery = GetSpellInfo(116781)
+local statscrit = GetSpellInfo(116781)
 
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
 	local function BuffCheck()
-		if self.db.profile.categories[L['Legacy']] == mastery then
-			return not GetRaidBuffTrayAuraInfo(8)
+		if self.db.profile.categories[L['Legacy']] == statscrit then
+			return not GetRaidBuffTrayAuraInfo(7)
 		elseif self.db.profile.categories[L['Legacy']] == stats then
 			return not GetRaidBuffTrayAuraInfo(1)
 		end
 	end
 	local buff = self:GetCategory(L['Legacy'])
 		:add( 115921, 'subcat', 'stats')   -- Legacy of the Emperor
-		:add( 116781, 'subcat', 'mastery') -- Legacy of the White Tiger
-	buff.checkRequirement = BuffCheck
+		:add( 116781, 'subcat', 'statscrit') -- Legacy of the White Tiger
+	buff.checkRequirement = BuffCheck	
 end)
