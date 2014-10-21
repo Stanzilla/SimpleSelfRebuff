2c918f53d7833024de20e179325c0884a82f3ad3
Benjamin Staneck <Stanzilla@users.noreply.github.com>
2014-10-21 11:43:37 +0000

    shout...shout! let it all out!

--- Buffs/warrior.lua
+++ Buffs/warrior.lua
@@ -3,10 +3,20 @@ local SimpleSelfRebuff = LibStub("AceAddon-3.0"):GetAddon("SimpleSelfRebuff", tr
 if not SimpleSelfRebuff then return end
 if select(2, UnitClass('player')) ~= 'WARRIOR' then return end
 
+local stamina = GetSpellInfo(469)
+local attackpower = GetSpellInfo(6673)
 
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
-	self:GetCategory(L['Shout']):addMulti(
-		6673, -- Battle Shout
-		469 -- Commanding Shout
-	)
+	local function Shout()
+		if self.db.profile.categories[L['Shout']] == stamina then
+			return not GetRaidBuffTrayAuraInfo(2)
+		elseif self.db.profile.categories[L['Shout']] == attackpower then
+			return not GetRaidBuffTrayAuraInfo(3)
+		end
+	end
+
+	local buff = self:GetCategory(L['Shout'])
+		:add( 6673, 'subcat', 'attackpower'     ) -- Battle Shout
+		:add( 469, 'subcat', 'stamina'     ) -- Commanding Shout
+	buff.checkRequirement = Shout			
 end)
