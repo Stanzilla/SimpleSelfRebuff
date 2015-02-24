d185a49ae46eb81def4c438c9fc4f591c0d65278
stanzilla <stanzilla@976ebace-77c9-427b-a127-ccb7ca2dd15e>
2015-02-24 15:20:17 +0000

    Update TOC for Patch 6.1

--- trunk/Buffs/warrior.lua
+++ trunk/Buffs/warrior.lua
@@ -7,14 +7,14 @@ local attackpower = GetSpellInfo(6673)
 SimpleSelfRebuff:RegisterBuffSetup(function(self, L)
 	local function Shout()
 		if self.db.profile.categories[L['Shout']] == stamina then
-			return not GetRaidBuffTrayAuraInfo(2)
+			return not (GetRaidBuffTrayAuraInfo(2))
 		elseif self.db.profile.categories[L['Shout']] == attackpower then
-			return not GetRaidBuffTrayAuraInfo(3)
+			return not (GetRaidBuffTrayAuraInfo(3))
 		end
 	end
 
 	local buff = self:GetCategory(L['Shout'])
 		:add( 6673, 'subcat', 'attackpower') -- Battle Shout
 		:add( 469, 'subcat', 'stamina') -- Commanding Shout
-	buff.checkRequirement = Shout			
+	buff.checkRequirement = Shout	
 end)
